04ab2e55649124f7b8bdc3bb2f5166153b7770c3
Paul Esch-Laurent <herppfel@gmail.com>
2017-05-03 01:47:39 -0400

    Whitelist font glob

--- template/gulp/tasks/build/fonts.js
+++ template/gulp/tasks/build/fonts.js
@@ -2,7 +2,7 @@ module.exports = (params) => {
   return () => {
     const {gulp, plugins, config, paths, production, root, server} = params;
 
-    return gulp.src(`${paths.src}/fonts/**`)
+    return gulp.src(`${paths.src}/fonts/**/*.+(eot|woff2|woff|ttf|svg)`)
       .pipe(gulp.dest(`${paths.tmp}/fonts`))
       .pipe(plugins.if(!production, server.stream()));
   };
