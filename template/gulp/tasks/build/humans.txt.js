12bec7f154e9e11d90877cfc0f5a9e8a7713c7f8
Paul Esch-Laurent <herppfel@gmail.com>
2017-05-03 22:36:49 -0400

    Add `gulp-update-humanstxt-date`

--- template/gulp/tasks/build/humans.txt.js
+++ template/gulp/tasks/build/humans.txt.js
@@ -3,6 +3,7 @@ module.exports = (params) => {
     const {gulp, plugins, config, paths, production, root, server, version, pkg} = params;
 
     return gulp.src(`./humans.txt`)
+      .pipe(plugins.updateHumanstxtDate())
       .pipe(gulp.dest(paths.dist));
   };
 };
